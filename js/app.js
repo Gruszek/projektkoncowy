03c7b0457c5e4f8dd1640f6d24e286186c507f2b
Gruszek <101717513+Gruszek@users.noreply.github.com>
2024-06-04 21:53:21 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,38 +1,46 @@
-var app = angular.module('portfolioApp', ['pascalprecht.translate', 'tmh.dynamicLocale']);
+// app.js
 
-app.config(['$translateProvider', '$translatePartialLoaderProvider', 'tmhDynamicLocaleProvider', function ($translateProvider, $translatePartialLoaderProvider, tmhDynamicLocaleProvider) {
-    // Konfiguracja Angular Translate
-    $translateProvider.useLoader('$translatePartialLoader', {
-        urlTemplate: '/translations/{lang}/{part}.json'
+angular.module('portfolioApp', ['pascalprecht.translate'])
+
+.config(function ($translateProvider) {
+    $translateProvider.translations('en', {
+        'HOME_TITLE': 'Home',
+        'HOME_DESC': 'Welcome to my portfolio',
+        'ABOUT_TITLE': 'About',
+        'ABOUT_DESC': 'I am a passionate web developer.',
+        'PROJECTS_TITLE': 'Projects',
+        'SKILLS_TITLE': 'Skills',
+        'CONTACT_TITLE': 'Contact',
+        'CONTACT_NAME': 'Name',
+        'CONTACT_EMAIL': 'Email',
+        'CONTACT_MESSAGE': 'Message',
+        'SEND_MESSAGE': 'Send Message'
     });
-    $translateProvider.preferredLanguage('en'); // Język domyślny
 
-    // Konfiguracja Dynamic Locale
-    tmhDynamicLocaleProvider.localeLocationPattern('/locales/angular-locale_{{locale}}.js');
-}]);
+    $translateProvider.translations('pl', {
+        'HOME_TITLE': 'Strona główna',
+        'HOME_DESC': 'Witaj na moim portfolio',
+        'ABOUT_TITLE': 'O mnie',
+        'ABOUT_DESC': 'Jestem pasjonatem tworzenia stron internetowych.',
+        'PROJECTS_TITLE': 'Projekty',
+        'SKILLS_TITLE': 'Umiejętności',
+        'CONTACT_TITLE': 'Kontakt',
+        'CONTACT_NAME': 'Imię',
+        'CONTACT_EMAIL': 'Email',
+        'CONTACT_MESSAGE': 'Wiadomość',
+        'SEND_MESSAGE': 'Wyślij wiadomość'
+    });
 
-// Kontroler dla strony
-app.controller('MainController', ['$scope', '$translate', function ($scope, $translate) {
-    // Dane projektów
+    $translateProvider.preferredLanguage('en');
+})
+
+.controller('MainController', function ($scope) {
     $scope.projects = [
-        { title: 'Project 1', description: 'Description of Project 1', image: 'img/project1.jpg' },
-        { title: 'Project 2', description: 'Description of Project 2', image: 'img/project2.jpg' }
+        { title: 'Project 1', description: 'Description of Project 1' },
+        { title: 'Project 2', description: 'Description of Project 2' },
+        { title: 'Project 3', description: 'Description of Project 3' }
+        // Dodaj więcej projektów według potrzeb
     ];
 
-    // Funkcja obsługi wysyłania wiadomości
-    $scope.sendMessage = function () {
-        // Logika wysyłania wiadomości
-        console.log('Wiadomość wysłana: ', $scope.contactName, $scope.contactEmail, $scope.contactMessage);
-        // Tutaj można dodać logikę wysyłania wiadomości przez API lub inne metody
-        // Resetowanie formularza
-        $scope.contactName = '';
-        $scope.contactEmail = '';
-        $scope.contactMessage = '';
-    };
-
-    // Funkcja zmiany języka
-    $scope.changeLanguage = function (key) {
-        $translate.use(key); // Ustawienie nowego języka
-    };
-}]);
-
+    $scope.skills = 'HTML, CSS, JavaScript, AngularJS';
+});
